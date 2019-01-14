e18539855521fdea89342b76e7889330d25ccc78
Brian Sayler <sayler.b@gmail.com>
2019-01-13 20:14:06 -0600

    smoke test for controller

---
+++ src/main/java/todo/TodoController.java
@@ -0,0 +1,7 @@
+package todo;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class TodoController {
+}
--- src/test/java/todo/ApplicationTest.java
+++ src/test/java/todo/ApplicationTest.java
@@ -1,7 +1,10 @@
 package todo;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,7 +12,11 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class ApplicationTest {
 
+    @Autowired
+    private TodoController controller;
+
     @Test
     public void shouldLoadTheApplicationContext() {
+        assertThat(controller).isNotNull();
     }
 }
