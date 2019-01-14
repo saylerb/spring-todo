881c8edde746694c74e8f0d037ffdea7a8259f1d
Brian Sayler <sayler.b@gmail.com>
2019-01-14 03:46:45 -0600

    add full http request test

--- src/main/java/todo/TodoController.java
+++ src/main/java/todo/TodoController.java
@@ -1,7 +1,14 @@
 package todo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class TodoController {
+
+    @RequestMapping(value = "/")
+    public @ResponseBody String hello() {
+        return "Hello, World!";
+    }
 }
---
+++ src/test/java/todo/HttpRequestTest.java
@@ -0,0 +1,30 @@
+package todo;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class HttpRequestTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void shouldHelloWorld() {
+        String url = "http://localhost:" + port + "/";
+
+        assertThat(this.restTemplate.getForObject(url, String.class)).contains("Hello, World!");
+    }
+
+}
