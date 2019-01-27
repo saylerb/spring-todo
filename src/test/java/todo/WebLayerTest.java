a197ce16fc68ce74e55d08c1ca9f365f59524844
Brian Sayler <sayler.b@gmail.com>
2019-01-26 19:33:56 -0600

    you can specify a single controller

--- src/test/java/todo/WebLayerTest.java
+++ src/test/java/todo/WebLayerTest.java
@@ -14,7 +14,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest
+@WebMvcTest(TodoController.class)
 public class WebLayerTest {
 
     @Autowired
