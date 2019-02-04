d0596cca4c429ff352769d633826036ef4ec7c86
Brian Sayler <sayler.b@gmail.com>
2019-02-03 18:20:00 -0600

    posting a todo integration test

--- src/test/java/todo/MockMvcTest.java
+++ src/test/java/todo/MockMvcTest.java
@@ -3,15 +3,19 @@ package todo;
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.options;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static todo.WebLayerTest.API_ROOT;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -40,5 +44,15 @@ public class MockMvcTest {
             .andExpect(status().isOk());
     }
 
-
+    @Test
+    public void shouldBeAbleToAddATodo() throws Exception {
+        this.mockMvc.perform(
+            post(API_ROOT)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{ \"title\": \"test todo\" }".getBytes())
+                .characterEncoding("utf-8"))
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.title").value("test todo"));
+    }
 }
