b1cf8a2a58ec617303b15bac5f4e9efd8d0b3cb1
Brian Sayler <sayler.b@gmail.com>
2019-05-05 00:43:54 -0500

    Use JPA repository instead of CRUD

--- src/main/java/todo/TodoRepository.java
+++ src/main/java/todo/TodoRepository.java
@@ -1,13 +1,9 @@
 package todo;
 
-import java.util.List;
-
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TodoRepository extends CrudRepository<Todo, Long> {
+public interface TodoRepository extends JpaRepository<Todo, Long> {
     Todo findByTitle(String title);
-
-    List<Todo> findAll();
 }
