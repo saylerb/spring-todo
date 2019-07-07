219bd8698756cebbdb815db6c673811625c03c71
Brian Sayler <sayler.b@gmail.com>
2019-07-07 18:33:12 -0500

    configure repository tests to NOT use embedded db

--- src/databaseTest/java/todo/RepositoryTest.java
+++ src/databaseTest/java/todo/RepositoryTest.java
@@ -1,6 +1,7 @@
 package todo;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace.NONE;
 
 import java.util.List;
 
@@ -8,12 +9,14 @@ import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = NONE)
 public class RepositoryTest {
 
     @Autowired
